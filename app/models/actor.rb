cc06f1322fe96b5491db07a9518c2be1be39504f
David Rafe <david.rafe@gmail.com>
2021-01-27 20:47:30 -0500

    commit tree e8adf8a06981f0b27a83871e74c5fd03f81b363f

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters 
+  has_many :shows, through: :characters 
+
+  def full_name
+    first_name = self.first_name
+    last_name = self.last_name
+    first_name + " " + last_name
+  end
+
+  def list_roles
+    # binding.pry
+    show = self.shows.map {|show| show.name}
+    character = self.characters.map {|character| character.name}
+    roles_string = "#{character.join()} - #{show.join()}"
+    roles_string.split(", ")
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters 
+  has_many :actors, through: :characters
+  belongs_to :network 
+
+  def actors_list 
+   actor_first_name = self.actors.map {|actor| actor.first_name}
+   actor_last_name = self.actors.map {|actor| actor.last_name}
+   actor_name_string = "#{actor_first_name.join()} #{actor_last_name.join()}"
+   actor_name_string.split.each_slice(2).map{|a|a.join ' '}
+  end
 end
